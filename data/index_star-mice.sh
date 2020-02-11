285eb8b0524ed9962afd4729b907653d46ee1972
hermuba <b101102109@tmu.edu.tw>
2020-02-11 15:11:47 -0800

    star aligner indexing script

--- data/index_star-mice.sh
+++ data/index_star-mice.sh
@@ -13,7 +13,7 @@
 STAR --runThreadN 16 \
 --outTmpDir ./_STARtmp-${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} \
 --runMode genomeGenerate \
---genomeDir /nrnb/users/hsher/star_hg38 \
---genomeFastaFiles /cellar/users/hsher/Data/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
---sjdbGTFfile /cellar/users/hsher/Data/reference/Homo_sapiens.GRCh38.98.gtf \
+--genomeDir /nrnb/users/hsher/star_mous \
+--genomeFastaFiles /cellar/users/hsher/Data/star_mus_musculus_index/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa \
+--sjdbGTFfile /cellar/users/hsher/Data/star_mus_musculus_index/Mus_musculus.GRCm38.98.gtf \
 --sjdbOverhang 99
